950b4a5de732f92a0c60fe449bd778fcd35d5e88
TRMES <142784774+TRMES@users.noreply.github.com>
2026-05-30 00:30:03 +0300

    Add files via upload

---
+++ Web Develop Codes/React/281-javascript-expressions-in-jsx/src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fname = "Emre";
+const lname = "Kenny"
+const number = 6;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {fname} {lname}!</h1>
+    <p>Your lucky number is {number}</p>
+  </div>,
+  document.getElementById("root"),
+);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
